2e599fd1f1e1719f4b9c4b92ab4124bde7d25c23
airewatterson <airewatterson@gmail.com>
2022-08-03 18:32:43 +0800

    navbar update

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
 <template>
-<router-view />
-<nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
-  <router-link to="/" class="flex items-center">
-      <img src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/rabbit_1f407.png" class="mr-3 h-6 sm:h-9" alt="Logo">
-  </router-link>
-  <div class="flex md:order-2">
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-  </div>
-  <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <router-link to="/" id="pages">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/about" id="pages">About</router-link>
-      </li>
-      <li>
-        <router-link to="/work" id="pages">My Work</router-link>
-      </li>
-      <li>
-        <router-link to="/contact" id="pages">Contact</router-link>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+<!-- navbar here -->
+<nav class="px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b">
+  <div class="max-w-6xl mx-auto px-4">
+    <div class="flex justify-between">
+      <div class="hidden md:flex space-x-4">
+        <!-- primary nav -->
+          <div div class="hidden md:flex items-center space-x-1">
+              <router-link to="/" class="py-5 px-3 text-gray-700 hover:text-black">Home</router-link>
+              <router-link to="/about" class="py-5 px-3 text-gray-700 hover:text-black">About Me</router-link>
+          </div>
+      </div>
+      <!-- logo -->
+          <div>
+            <router-link to="/" class="flex items-center py-3 px-2 text-gray-700 hover:text-black">
+              <img src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/rabbit_1f407.png" 
+            alt="logo" class="h-12 w-12">
 
+            <!-- <span class="font-bold">Aire Watterson</span> -->
+            </router-link>
+        </div>
+      <!-- secondary nav -->
+      <div class="hidden md:flex items-center space-x-1">
+        <router-link to="/work" class="py-5 px-3 text-gray-700 hover:text-black">My Work</router-link>
+        <router-link to="/contact" class="py-5 px-3 text-white bg-blue-600 hover:bg-blue-400 transition duration-150 rounded shadow">Contact</router-link>
+      </div>
+      <!-- mobile button -->
+      <div class="md:hidden flex items-center">
+        <button class="mobile-menu-button">
+          <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>Menu</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M80 160h352M80 256h352M80 352h352"/></svg>
+        </button>
+      </div>
+    </div>
+  </div>
+
+
+
+<!-- mobile menu -->
+<div class="hidden mobile-menu md:hidden">
+  <router-link to="/" class="block py-4 px-4 text-sm hover:bg-gray-200">Home</router-link>
+  <router-link to="/about" class="block py-4 px-4 text-sm hover:bg-gray-200">About Me</router-link>
+  <router-link to="/work" class="block py-4 px-4 text-sm hover:bg-gray-200">My Work</router-link>
+  <router-link to="/contact" class="block py-4 px-4 text-sm hover:bg-gray-200">Contact</router-link>
+</div>
+
+</nav>
+<router-view />
 </template>
 
 <style>
@@ -48,5 +58,13 @@
 </style>
 
 <script>
-
-</script>
+//以下是自適應導航列程式
+  //指定所有變數
+  const btn=document.querySelector("button.mobile-menu-button");
+  const menu=document.querySelector(".mobile-menu");
+  
+  //event listeners
+  btn.addEventListener("click",()=>{
+    menu.classList.toggle("hidden");
+  });
+</script> 
